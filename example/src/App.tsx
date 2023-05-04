c4a94bb05c0398387817da3de909bbdbc69ab039
Quan Pham <doraemonfanclub@gmail.com>
2023-05-04 17:19:18 +0700

    Initializing

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-matrix-universal-sdk';
+import { multiply } from '../../src';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
