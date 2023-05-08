55b3d5b9c32494bfc4671b608ab36e768b868b09
Quan Pham <doraemonfanclub@gmail.com>
2023-05-08 15:20:00 +0700

    Backup iOS code

---
+++ ios/Utilities.h
@@ -0,0 +1,19 @@
+//
+//  Utilities.h
+//  MatrixUniversalSdk
+//
+//  Created by Quan Pham on 08/05/2023.
+//  Copyright © 2023 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Utilities : NSObject
+
++ (NSData *)dataFromHexString:(NSString *)string;
+
+@end
+
+NS_ASSUME_NONNULL_END
