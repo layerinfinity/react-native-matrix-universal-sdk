5a61cdadcd3a5c052cefa1c144c8efcd030ede02
Quan Pham <doraemonfanclub@gmail.com>
2023-06-20 12:33:32 +0700

    Update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text, Button } from 'react-native';
-import { createClient } from '../../';
+import { createClient, login } from '../../';
 
 export default function App() {
   const [result] = React.useState<number | undefined>();
 
-  async function login() {
+  async function loggg() {
     const resp = await createClient({
       homeServerUrl: 'https://matrix.tauhu.cloud/',
+    });
+    console.log(resp);
+  }
+
+  async function loginWithPw() {
+    const resp = await login({
+      username: 'gm.doko',
+      password: 'test123456',
     });
     console.log(resp);
   }
@@ -17,7 +25,9 @@ export default function App() {
     <View style={styles.container}>
       <Text>Result: {result}</Text>
 
-      <Button title="Log" onPress={login} />
+      <Button title="Log" onPress={loggg} />
+
+      <Button title="Log2" onPress={loginWithPw} />
     </View>
   );
 }
